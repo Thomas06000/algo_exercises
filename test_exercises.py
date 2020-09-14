7ae069170377527b6416bb12170ae5a9c5f1af3e
Thomas Auger <thomas.auger@laddition.com>
2020-09-14 18:05:18 +0200

    clean up

--- test_exercises.py
+++ test_exercises.py
@@ -4,13 +4,11 @@ import exercises
 class TestAlgorithmExercises(unittest.TestCase):
 
     def test_sumUpToK(self):
-        # When
         subject = exercises.sumUpToK([1, 2], 100)
         subject2 = exercises.sumUpToK([1, 99], 100)
         subject3 = exercises.sumUpToK([], 100)
         subject4 = exercises.sumUpToK([5, 4, 1, -3, 6], 7)
 
-        # Then
         self.assertFalse(subject)
         self.assertTrue(subject2)
         self.assertFalse(subject3)
